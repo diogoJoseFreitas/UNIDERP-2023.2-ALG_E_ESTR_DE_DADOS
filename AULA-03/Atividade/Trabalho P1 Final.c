a2fe1849212e5e17d4836cf2cfc1393bb8095010
Diogo Oliveira <djfreitasoliveira@gmail.com>
2023-08-31 21:58:04 -0400

    file: save files

---
+++ AULA-03/Atividade/Trabalho P1 Final.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <locale.h>
+
+float salario(float n1, float n2) // fun��o para calculo do salario bruto
+{
+	float resultado = n1 * n2;
+	return (resultado);
+}
+float inss(float n1) // fun��o para calculo do inss
+{
+	float resultadoi = n1 * 0.1;
+	return (resultadoi);
+}
+float fgts(float n1) // fun��o para calculo do fgts
+{
+	float resultadof = n1 * 0.11;
+	return (resultadof);
+}
+float ir(float n1) // fun��o para calculo do imposto de renda
+{
+	float imposto = n1 * 0.1;
+	return (imposto);
+}
+float descontos(float n1, float n2) // fun��o para calculo dos descontos
+{
+	float desconto = n1 + n2; 
+	return (desconto);
+}
+float liquido(float n1, float n2) // fun��o para calculo do salario liquido
+{
+	float resultadol = n1 - n2;
+	return (resultadol);
+}
+int main(){
+	setlocale(LC_ALL, "");
+	float a, b, resultado, imposto=0, resultadoi, resultadol, desconto, resultadof;
+
+	printf("Informe a quantidade de horas trabalhadas: \n");
+	scanf("%f", &a);
+	
+	printf("Informe o valor por hora trabalhada: \n");
+	scanf("%f", &b);	
+	
+	resultado = salario(a, b); // função onde é calculado o salario do trabalhador, horas trabalhadas x valor da hora
+	
+	
+	if(resultado>1000){ // aqui ele vai testar se o resultado do salario é maior que mil, se for ele vai calcular o imposto de renda
+		imposto = ir(resultado); // função onde é calculado o imposto de renda (10%) a ser descontado se o salário bruto passar dos mil reais
+	}
+	else{
+		printf("\n Não é nescessário descontar o imposto de renda:"); //caso o valor seja abaixo de mil reais, não é calculado o imposto de renda
+	}
+	
+	resultadoi = inss(resultado); // função onde é calculado o valor do INSS(10%) a ser descontado do salário bruto
+	resultadof = fgts(resultado); // função onde é calculado o valor do FGTS(11%) a ser descontado do salário bruto
+	desconto = descontos(imposto, resultadoi); //função onde é calculado o total de descontos
+	resultadol = liquido(resultado, desconto); //função onde é calculado o salário liquido, realizando a subtração entre o salário bruto e os descontos
+	
+	printf("\n O salario bruto é: R$ %.2f", resultado); //imprime o salario bruto
+	printf("\n (-) IR (10%%): R$ %.2f", imposto); //imprime o valor do imposto de renda a ser descontado
+	printf("\n (-) INSS (10%%): R$ %.2f", resultadoi);  //imprime o valor do inss a ser descontado
+	printf("\n FGTS (11%%): R$ %.2f", resultadof); //imprime o valor do fgts a ser descontado
+	printf("\n Total de descontos: R$ %.2f", desconto);  //imprime o total de descontos
+	printf("\n Salário liquido: R$ %.2f", resultadol);  //imprime o salario liquido
+	
+	return 0;
+}
---
+++ AULA-04/Aula/aula-04.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+struct Lista
+{
+    int num;
+    struct Lista *prox;
+};
+typedef struct Lista node;
+
+
+void adicionaNode (node* cabeca)
+{
+    node *temp = cabeca;
+    node *noh = (node*) malloc(sizeof(node));
+
+    if (!noh){
+        printf("Sem memoria suficiente");
+        exit(1);
+    }
+
+    printf("Informe o valor Inteiro: ");
+    scanf("%d", &noh->num);
+    noh->prox = NULL;
+
+    while(temp->prox != NULL){
+        temp = temp->prox;
+    }
+
+    temp->prox = noh;
+}
+
+void imprimeNode(node* cabeca)
+{
+    node *temp = cabeca;
+    while (temp->prox != NULL)
+    {
+        temp = temp->prox;
+        printf(" - %d\n", temp->num);
+    }
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    node cab;
+    cab.prox = NULL;
+
+    printf("Quantos números você deseja inserir? ");
+    scanf("%d", &i);
+
+
+    for(i; i<10; i++){
+        adicionaNode(&cab);
+    }
+    imprimeNode(&cab);
+
+    return 0;
+}
+
